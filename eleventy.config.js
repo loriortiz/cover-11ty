13e4c0d15ef4d40b099cca907b5d8d342ae2ae66
lorio <loriortiz13@gmail.com>
2023-05-29 18:19:44 -0400

    Fix path to icon

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,8 +14,7 @@ module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
-		"./public/": "/",
-		"./img/": "/"
+		"./public/": "/"
 	});
 
 	// Run Eleventy when these files change:
