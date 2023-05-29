cb700b77bba5f7ffe3ee50fff3578a7049c7ac13
lorio <loriortiz13@gmail.com>
2023-05-29 17:39:37 -0400

    Add path to icon

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,7 +15,7 @@ module.exports = function(eleventyConfig) {
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
 		"./public/": "/",
-		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
+		"./img/": "/"
 	});
 
 	// Run Eleventy when these files change:
